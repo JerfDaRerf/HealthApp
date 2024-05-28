de9d2c71a67be2fdb500ce37f7d8f7cad4084a4f
Jeff300fang <jeff300fang@gmail.com>
2024-05-27 21:20:35 -0400

    switch to list view

--- fitnessapp/lib/views/navigation_pages/workout_page.dart
+++ fitnessapp/lib/views/navigation_pages/workout_page.dart
@@ -73,7 +73,7 @@ class _WorkoutPage extends State<WorkoutPage>{
                     child: workoutTitle(),
                   ),
                   Expanded(
-                    child: SuggestedExercises(),
+                    child: SuggestedExercisesList(), //SuggestedExercisesGrid(),
                   ),
               
                 ],
@@ -271,7 +271,117 @@ class _WorkoutPage extends State<WorkoutPage>{
 }
 
 
-class SuggestedExercises extends StatelessWidget {
+class SuggestedExercisesList extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index){
+        return Column(
+          children: <Widget>[
+            Row(
+              children: [
+                gifBorder(exerciseGIF()),
+                Expanded(
+                  child: exerciseDescription()
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: checkIcon(),
+                ),
+                infoIcon(),
+              ],
+            ),
+            // const SizedBox(height: 10.0),
+            Container(
+              height: 0.7, 
+              width: double.infinity,
+              color: Colors.grey, 
+              margin: const EdgeInsets.fromLTRB(100.0, 5.0, 0.0, 5.0),
+            ),
+          ],
+        );
+      }
+    );
+  }
+
+  Icon checkIcon() {
+    return const Icon(
+      Icons.add_box_outlined,
+      color: Colors.grey,
+      size: 26,
+    );
+  }
+
+  Icon infoIcon() {
+    return const Icon(
+      Icons.info_outline_rounded,
+      color: Colors.grey,
+      size: 26,
+    );
+  }
+
+  Padding exerciseDescription() {
+    return const Padding(
+      padding: EdgeInsets.only(left: 8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget> [
+          Text(
+            'Weighted Crunch',
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+            ),
+            textAlign: TextAlign.left,
+          ),
+          Text(
+            'Abs',
+            style: TextStyle(
+              fontSize: 15.0,
+              color: Colors.grey,
+              fontWeight: FontWeight.w400,
+            ),
+            textAlign: TextAlign.left,
+          )
+        ]
+      ),
+    );
+  }
+
+  Container gifBorder(Widget gif) {
+    return Container(
+      width: 90.0,
+      height: 90.0,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        // border: Border.all(
+        //   color: Colors.grey,
+        //   width: 0.3,
+        // ),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: gif,
+      ),
+    );
+  }
+  
+  ClipRRect exerciseGIF() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10.0),
+      child: Image.asset(
+        'lib/exercises/12501301-Weighted-Crunch-(behind-head)_Waist_360.gif'
+      ),
+    );
+  }
+
+}
+
+class SuggestedExercisesGrid extends StatelessWidget {
   final List<int> items = List<int>.generate(1000, (i) => i);
 
   @override
@@ -279,58 +389,78 @@ class SuggestedExercises extends StatelessWidget {
     return GridView.builder(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 3,
-        crossAxisSpacing: 15.0,
-        mainAxisSpacing: 15.0,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
         childAspectRatio: 1.0,
       ),
       itemCount: 5,
       itemBuilder: (context, index){
-        return Container(
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            // border: Border.all(
-            //   color: Colors.grey,
-            //   width: 4.0,
-            // ),
-            color: Colors.grey[200],
-            borderRadius: BorderRadius.circular(16.0),
-          ),
-          child: Stack(
-            children: <Widget>[ 
-              ClipRRect(
-                // borderRadius: BorderRadius.circular(20.0),
-                child: Image.asset(
-                  'lib/exercises/12501301-Weighted-Crunch-(behind-head)_Waist_360.gif'
-                ),
+        return Material(
+          elevation: 3.0,
+          borderRadius: BorderRadius.circular(10.0),
+          child: Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey,
+                width: 0.3,
               ),
-              const Align(
-                alignment: Alignment.bottomLeft,
-                child: Padding(
-                  padding: EdgeInsets.only(left: 14.0),
-                  child: Text(
-                    'Abs',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
+              // color: const Color.fromRGBO(205, 232, 229, 0.6),
+              // color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Stack(
+              children: <Widget>[ 
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: exerciseGIF(),
                 ),
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 4.0, right: 4.0),
-                  child: Icon(
-                    Icons.info_outline_rounded,
-                    color: Colors.black.withOpacity(0.7),
-                  ),
-                ),
-              )
-            ]
+                //exerciseDescription(),
+                infoIcon()
+              ]
+            ),
           ),
         );
       }
+    );
+  }
+
+  ClipRRect exerciseGIF() {
+    return ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Image.asset(
+                  'lib/exercises/12501301-Weighted-Crunch-(behind-head)_Waist_360.gif'
+                ),
+              );
+  }
+
+  Align exerciseDescription() {
+    return const Align(
+      alignment: Alignment.bottomLeft,
+      child: Padding(
+        padding: EdgeInsets.only(left: 14.0),
+        child: Text(
+          'Abs',
+          style: TextStyle(
+            fontSize: 16.0,
+            color: Colors.black,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+  
+  Align infoIcon() {
+    return const Align(
+      alignment: Alignment.topRight,
+      child: Padding(
+        padding: EdgeInsets.only(top: 4.0, right: 4.0),
+        child: Icon(
+          Icons.info_outline_rounded,
+          color: Colors.grey,
+        ),
+      ),
     );
   }
 }
